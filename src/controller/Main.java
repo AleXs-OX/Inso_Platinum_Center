bff1624b6eb53b5d84188bb157b90aca488efb0f
AleXs-OX <agonzm32@estudiantes.unileon.es>
2022-01-01 21:49:11 +0100

    Changes in main class, incialice windows

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,4 +1,23 @@
 package controller;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/view/LoginUI.fxml"));
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(new Scene(root, 640.0, 400));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }

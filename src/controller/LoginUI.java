71cb4806ef6068170e41fba1f4fbc70ea4c0c3a0
AleXs-OX <agonzm32@estudiantes.unileon.es>
2022-01-02 18:44:56 +0100

    En proceso de crear la siguiente ventana a la de login

--- src/controller/LoginUI.java
+++ src/controller/LoginUI.java
@@ -2,9 +2,14 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginUI {
 
@@ -17,20 +22,47 @@ public class LoginUI {
     @FXML
     private TextField textFiledUser;
 
+    private Stage primaryStage;
     //@FXML
     //void buttonLoginPressed(ActionEvent event) {
 
     //}
 
     public void initialize(){
-        buttonLogin.setOnAction(e -> this.entrar());
+        buttonLogin.setOnAction(e -> {
+            try {
+                this.entrar();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
     }
 
-    public void entrar(){
+    //Faltaria cambiar la excepcion a un try catch en el metodo entrar, no en el metodo initialize
+    public void entrar() throws IOException {
+        FXMLLoader newRoot;
+
         System.out.println("Prueba de boton");
+        String adm = "Admin";
+        String empleado = "Empleado";
+        String cliente = "Cliente";
 
         System.out.println("Usuario: "+ textFiledUser.getText().toString());
         System.out.println("Contraseña: "+ textFieldPass.getText().toString());
+
+        if(adm.equals(textFiledUser.getText().toString())){
+
+            newRoot = new FXMLLoader(getClass().getResource("/view/Administrator.fxml"));
+            Parent root = (Parent)newRoot.load();
+            AdministratorUI admin = newRoot.<AdministratorUI>getController();
+            primaryStage.getScene().setRoot(root);
+
+        }else if(empleado.equals(textFiledUser.getText().toString())){
+
+        }else if(cliente.equals(textFiledUser.getText().toString())){
+
+        }
     }
+
 
 }

89ab2c1e23f47b0ac6213e8c5146045b76e767b2
AleXs-OX <agonzm32@estudiantes.unileon.es>
2022-01-03 01:53:19 +0100

    Conseguido porfin el swiching de ventanas, falta dejarlo bonito (La estructura esta)

--- src/controller/LoginUI.java
+++ src/controller/LoginUI.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -23,43 +24,47 @@ public class LoginUI {
     private TextField textFiledUser;
 
     private Stage primaryStage;
+    //FXMLLoader newRoot;
+
     //@FXML
     //void buttonLoginPressed(ActionEvent event) {
 
     //}
 
     public void initialize(){
-        buttonLogin.setOnAction(e -> {
-            try {
-                this.entrar();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        });
+            this.buttonLogin.setOnAction(e -> {
+                try {
+                    this.entrar();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            });
     }
 
     //Faltaria cambiar la excepcion a un try catch en el metodo entrar, no en el metodo initialize
     public void entrar() throws IOException {
-        FXMLLoader newRoot;
 
         System.out.println("Prueba de boton");
         String adm = "Admin";
         String empleado = "Empleado";
         String cliente = "Cliente";
 
-        System.out.println("Usuario: "+ textFiledUser.getText().toString());
-        System.out.println("Contraseña: "+ textFieldPass.getText().toString());
+        System.out.println("Usuario: "+ textFiledUser.getText());
+        System.out.println("Contraseña: "+ textFieldPass.getText());
 
-        if(adm.equals(textFiledUser.getText().toString())){
+        if(adm.equals(textFiledUser.getText())){
 
-            newRoot = new FXMLLoader(getClass().getResource("/view/Administrator.fxml"));
-            Parent root = (Parent)newRoot.load();
-            AdministratorUI admin = newRoot.<AdministratorUI>getController();
-            primaryStage.getScene().setRoot(root);
+            Parent newRoot = FXMLLoader.load(getClass().getResource("/view/Administrator.fxml"));
+            primaryStage = (Stage) this.buttonLogin.getScene().getWindow();
 
-        }else if(empleado.equals(textFiledUser.getText().toString())){
+            primaryStage.getScene().setRoot(newRoot);
+            //AdministratorUI admin = newRoot.<AdministratorUI>getController();
+            //stage.setScene(scene);
+            //stage.show();
 
-        }else if(cliente.equals(textFiledUser.getText().toString())){
+        }else if(empleado.equals(textFiledUser.getText())){
+
+        }else if(cliente.equals(textFiledUser.getText())){
 
         }
     }

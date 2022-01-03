413102d264970070139a3eaf4ab5ce5c90719998
AleXs-OX <agonzm32@estudiantes.unileon.es>
2022-01-03 13:47:35 +0100

    Clase SQL Controller hecha, (Conexion con la base de datos)

--- src/model/connection/SQL_Controller_Conexion.java
+++ src/model/connection/SQL_Controller_Conexion.java
@@ -1,4 +1,41 @@
 package model.connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class SQL_Controller_Conexion {
+
+    /*Variables used to connect to the database*/
+
+    private String url = "";
+    private String user = "";
+    private String password = "";
+
+    private Connection connection = null;
+
+    public SQL_Controller_Conexion(){
+
+
+        try {
+            /*Connection with the database*/
+            connection = DriverManager.getConnection(url, user, password);
+
+            if (connection != null) {
+                System.out.println("Conexion exitosa con la base de datos");
+            }
+
+        }catch (SQLException e) {
+            e.printStackTrace();
+            // handler any errors
+            System.out.println("ERROR DATABASE CONNECTION SQLException: " + e.getMessage());
+            System.out.println("ERROR DATABASE CONNECTION SQLState: " + e.getSQLState());
+            System.out.println("ERROR DATABASE CONNECTION VendorError: " + e.getErrorCode());
+        }
+    }
+    /*Method returns the connection*/
+    public Connection getConnection(){
+        return connection;
+    }
+
 }

6dc5c8bf49ec7eba0cfbca20abae9dad12487630
JRamos04 <jramom04@estudiantes.unileon.es>
2022-02-01 03:20:48 +0100

    Implementados DAO de usuarios

--- src/model/dao/EmployeeDao.java
+++ src/model/dao/EmployeeDao.java
@@ -1,6 +1,117 @@
 package model.dao;
 
 import model.connection.SQL_Controller_Conexion;
+import model.vo.EmployeeVo;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmployeeDao extends SQL_Controller_Conexion{
+
+    public List<EmployeeVo> listar() throws Exception{
+
+        ArrayList<EmployeeVo> listaEmpleados = new ArrayList<EmployeeVo>();
+        try{
+            this.openConnection();
+
+            PreparedStatement st = this.getConnection().prepareStatement("SELECT * from empleados");
+            ResultSet result = st.executeQuery();
+
+            while (result.next()){
+
+                EmployeeVo empleado = new EmployeeVo();
+                empleado.setIdEmpleado(result.getInt("idEmpleado"));
+                empleado.setFechaContratacion(result.getDate("fechaContratacion"));
+                empleado.setFechaDespido(result.getDate("fechaDespido"));
+                empleado.setIdSalario(result.getInt("idSalario"));
+
+                listaEmpleados.add(empleado);
+            }
+        }
+        catch(Exception e){
+            throw new Exception("Listar Empleados "+e.getMessage());
+        }finally {
+            try {
+                this.closeConnection();
+            } catch (Exception e) {
+                throw new Exception("Error al cerrar la conexion listar empleados: " + e.getMessage());
+            }
+        }
+        return listaEmpleados;
+    }
+
+    public EmployeeVo buscar(int idEmpleado) throws Exception {
+
+        EmployeeVo empleado = new EmployeeVo();
+        String criterioBusqueda = '%' + Integer.toString(idEmpleado) + '%';
+
+        try {
+            this.openConnection();
+
+            PreparedStatement st = this.getConnection().prepareStatement("SELECT * FROM empleados WHERE idEmpleado "
+                    + "LIKE ?");
+            st.setString(1, criterioBusqueda);
+            ResultSet result = st.executeQuery();
+
+            while(result.next()) {
+                empleado.setIdEmpleado(result.getInt("idEmpleado"));
+                empleado.setFechaContratacion(result.getDate("fechaContratacion"));
+                empleado.setFechaDespido(result.getDate("fechaDespido"));
+                empleado.setIdSalario(result.getInt("idSalario"));
+            }
+
+        }catch (Exception e){
+            throw new Exception("Error al buscar empleado: " + e.getMessage());
+        }finally {
+            try {
+                this.closeConnection();
+            }catch(Exception e) {
+                throw new Exception("Error al cerrar la conexion buscar empleado: " + e.getMessage());
+            }
+        }
+
+        return empleado;
+    }
+
+    public void eliminar(EmployeeVo empleado) throws Exception{
+        try{
+            this.openConnection();
+
+            PreparedStatement st = this.getConnection().prepareStatement("DELETE FROM empleados WHERE idEmpleado=?");
+            st.setInt(1, empleado.getIdEmpleado());
+            st.executeUpdate();
+        }
+        catch(Exception e){
+            throw new Exception("Eliminar Empleado "+e.getMessage());
+        }finally {
+            try {
+                this.closeConnection();
+            } catch (Exception e) {
+                throw new Exception("Error al cerrar la conexion eliminar empleado: " + e.getMessage());
+            }
+        }
+    }
+
+    public void registrar(EmployeeVo empleado) throws Exception{
+        try{
+            this.openConnection();
+
+            PreparedStatement st = this.getConnection().prepareStatement("INSERT INTO empleados (idEmpleado, fechaContratacion, fechaDespido, idSalario)" +
+                    "VALUES (?,?,?,?)");
+            st.setInt(1, empleado.getIdEmpleado());
+            st.setDate(2, empleado.getFechaContratacion());
+            st.setDate(3, empleado.getFechaDespido());
+            st.setInt(4, empleado.getIdSalario());
+        }
+        catch(Exception e){
+            throw new Exception("Registrar Empleado "+e.getMessage());
+        }finally {
+            try {
+                this.closeConnection();
+            } catch (Exception e) {
+                throw new Exception("Error al cerrar la conexion registrar empleado: " + e.getMessage());
+            }
+        }
+    }
 }
--- src/model/dao/UsersDao.java
+++ src/model/dao/UsersDao.java
@@ -9,15 +9,14 @@ import java.util.List;
 
 public class UsersDao extends SQL_Controller_Conexion {
 
-    public UsersDao() {
-        new SQL_Controller_Conexion();
-    }
-
     public List<UsersVo> listar() throws Exception{
+
         ArrayList<UsersVo> listaUsuarios = new ArrayList<UsersVo>();
         try{
-            PreparedStatement statement = this.getConnection().prepareStatement("SELECT * from usuarios");
-            ResultSet result = statement.executeQuery();
+            this.openConnection();
+
+            PreparedStatement st = this.getConnection().prepareStatement("SELECT * from usuarios");
+            ResultSet result = st.executeQuery();
 
             while (result.next()){
                 UsersVo user = new UsersVo();
@@ -32,34 +31,91 @@ public class UsersDao extends SQL_Controller_Conexion {
         }
         catch(Exception e){
             throw new Exception("Listar Usuarios "+e.getMessage());
+        }finally {
+            try {
+                this.closeConnection();
+            } catch (Exception e) {
+                throw new Exception("Error al cerrar la conexion listar usuarios: " + e.getMessage());
+            }
         }
         return listaUsuarios;
     }
 
+    public UsersVo buscar(int idUser) throws Exception {
+
+        UsersVo user = new UsersVo();
+        String criterioBusqueda = '%' + Integer.toString(idUser) + '%';
+
+        try {
+            this.openConnection();
+
+            PreparedStatement st = this.getConnection().prepareStatement("SELECT * FROM usuarios WHERE idUsuario "
+                    + "LIKE ?");
+            st.setString(1, criterioBusqueda);
+            ResultSet result = st.executeQuery();
+
+            while(result.next()) {
+                user.setIdUsuario(result.getInt("idUsuario"));
+                user.setNombreCompleto(result.getString("nombreCompleto"));
+                user.setNombreUsuario(result.getString("nombreUsuario"));
+                user.setContrasena(result.getString("contrasena"));
+                user.setFechaNacimiento(result.getDate("fechaNacimiento"));
+                user.setTipoDeUsuario(result.getInt("tipoUsuario"));
+            }
+
+        }catch (Exception e){
+            throw new Exception("Error al buscar usuario: " + e.getMessage());
+        }finally {
+            try {
+                this.closeConnection();
+            }catch(Exception e) {
+                throw new Exception("Error al cerrar la conexion buscar usuario: " + e.getMessage());
+            }
+        }
+
+        return user;
+    }
+
     public void eliminar(UsersVo user) throws Exception{
         try{
-            PreparedStatement statement = this.getConnection().prepareStatement("DELETE FROM usuarios WHERE IdUsuario=?");
-            statement.setInt(1, user.getIdUsuario());
-            statement.executeUpdate();
+            this.openConnection();
+
+            PreparedStatement st = this.getConnection().prepareStatement("DELETE FROM usuarios WHERE IdUsuario=?");
+            st.setInt(1, user.getIdUsuario());
+            st.executeUpdate();
         }
         catch(Exception e){
-            throw new Exception("Eliminar Usuarios "+e.getMessage());
+            throw new Exception("Eliminar Usuario "+e.getMessage());
+        }finally {
+            try {
+                this.closeConnection();
+            } catch (Exception e) {
+                throw new Exception("Error al cerrar la conexion eliminar usuario: " + e.getMessage());
+            }
         }
     }
 
     public void registrar(UsersVo user) throws Exception{
         try{
-            PreparedStatement statement = this.getConnection().prepareStatement("INSERT INTO usuarios (idUsuario, nombreCompleto, nombreUsuario, contrasena, fechaNacimiento, tipoDeUsuario)" +
+            this.openConnection();
+
+            PreparedStatement st = this.getConnection().prepareStatement("INSERT INTO usuarios (idUsuario, nombreCompleto, nombreUsuario, contrasena, fechaNacimiento, tipoDeUsuario)" +
                     "VALUES (?,?,?,?,?,?)");
-            statement.setInt(1, user.getIdUsuario());
-            statement.setString(2, user.getNombreCompleto());
-            statement.setString(3, user.getNombreUsuario());
-            statement.setString(3, user.getContrasena());
-            statement.setDate(4, user.getFechaNacimiento());
-            statement.setInt(5, user.getTipoDeUsuario());
+            st.setInt(1, user.getIdUsuario());
+            st.setString(2, user.getNombreCompleto());
+            st.setString(3, user.getNombreUsuario());
+            st.setString(3, user.getContrasena());
+            st.setDate(4, user.getFechaNacimiento());
+            st.setInt(5, user.getTipoDeUsuario());
         }
         catch(Exception e){
-            throw new Exception("Eliminar Usuarios "+e.getMessage());
+            throw new Exception("Registrar Usuario "+e.getMessage());
+        }finally {
+            try {
+                this.closeConnection();
+            } catch (Exception e) {
+                throw new Exception("Error al cerrar la conexion registrar usuario: " + e.getMessage());
+            }
         }
     }
 }

f443e81119560a958a927b9d3fa6d589918c49d2
jgomem04 <jgomem04@estudiantes.unileon.es>
2022-02-06 18:39:06 +0100

    More changes in login logic

--- src/model/loginLogic.java
+++ src/model/loginLogic.java
@@ -5,49 +5,24 @@ import model.vo.UsersVo;
 
 public class loginLogic {
 	
-	private static loginLogic instancia;
-	private int idUsuario;
+	public loginLogic() {
+		super();
+	}
 	
-	private loginLogic() {}
-	
-	public int iniciarSesion(String usuario, String password) throws Exception{
-		int tipoUsuario = -1;
+	public UsersVo iniciarSesion(String usuario, String password) throws Exception{
+		UsersVo vo = new UsersVo();
 		
 		if(usuario == null || password == null || usuario == "" || password == "" ) {
 			throw new Exception("Alguno de los campos está vacío. Rellénalos.");
 		}else {
 			try {
-				UsersVo vo = new UsersVo();
 				UsersDao dao = new UsersDao();
 				vo = dao.buscar(usuario, password);
-				
-				setIdUsuario(vo.getIdUsuario());
-				tipoUsuario = vo.getTipoDeUsuario();
 			}catch(Exception e) {
 				throw new Exception("Usuario y/o contraseña incorrectos. Error: " + e.getMessage());
 			}
 		}
 		
-		return tipoUsuario;
-	}
-	
-	public static loginLogic getInstancia() {
-		if(instancia == null) {
-			instancia = new loginLogic();
-		}
-		
-		return instancia;
-	}
-	
-	public int getIdUsuario() {
-		return idUsuario;
-	}
-	
-	public void logout() {
-		this.idUsuario = -1;
-	}
-	
-	private void setIdUsuario(int idUsuario) {
-		this.idUsuario = idUsuario;
+		return vo;
 	}
 }

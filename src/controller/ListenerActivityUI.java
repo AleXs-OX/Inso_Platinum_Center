fd9411b42510faaec0f216f9bd1396b5919f37e2
AleXs-OX <agonzm32@estudiantes.unileon.es>
2022-02-08 14:36:57 +0100

    cambiada tabla de actividades

--- src/controller/ListenerActivityUI.java
+++ src/controller/ListenerActivityUI.java
@@ -3,37 +3,48 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.util.LinkedList;
 
 public class ListenerActivityUI {
 
     @FXML
     private VBox itemsVbox = null;
+    @FXML
+    private HBox itemHbox;
 
 
-    public void initializable(){
+    public void initialize(){
 
         System.out.println("Activando tabla");
-        this.rellaVbox();
+        this.rellenaVbox();
     }
 
 
-    public void rellaVbox(){
+    public void rellenaVbox(){
 
-    Node[] nodes = new Node[10];
+        Node[] nodes = new Node[10];
 
-    for(int i = 0; i < nodes.length; i++){
+        for (int i = 0; i < nodes.length; i++) {
+            try {
 
-       try{
-            nodes[i] = FXMLLoader.load(getClass().getResource("view/itemDashBoard.fxml"));
-            itemsVbox.getChildren().add(nodes[i]);
+                nodes[i] = FXMLLoader.load(getClass().getResource("/view/itemDashBoard.fxml"));
+                itemsVbox.getChildren().add(nodes[i]);
 
-       }catch (IOException e){
-           e.printStackTrace();
-       }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
+
+    private void rellenaNodes(){
+
+
 
     }
 }
